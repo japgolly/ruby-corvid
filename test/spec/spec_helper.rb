a9544d0b1bc8b3ecd1294c10604aa39d049b10f8
David Barri <japgolly@gmail.com>
2012-07-23 23:59:53 +1000

    Tests now patch .corvid/Gemfile instead of older Gemfile.corvid

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -64,7 +64,7 @@ module TestHelpers
   end
 
   def patch_corvid_gemfile
-    files= %w[Gemfile Gemfile.corvid]
+    files= %w[Gemfile .corvid/Gemfile]
     files.select!{|f| File.exists? f}
     unless files.empty?
       `perl -pi -e '
