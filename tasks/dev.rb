7a258b136e445c06c75eba8e2d35b11b2285d150
David Barri <japgolly@gmail.com>
2012-08-04 18:21:29 +1000

    Getting travis working with its one set of gems (without running bundle all the time)

--- tasks/dev.rb
+++ tasks/dev.rb
@@ -4,18 +4,18 @@ namespace :dev do
   task :fixture_gems do
     ENV['BUNDLE_GEMFILE']= nil
     ENV['RUBYOPT']= nil
+    CORVID_ROOT ||= File.expand_path('../..',__FILE__)
 
     # Gemfile.lock
-    system %q[sed -n 's/^ *\|#.*//g; /yard/d; /^gem .*git:/w gem.tmp' Gemfile]
+    system %q[sed -n 's/^ *\|#.*//g; /^gem .*git:/w gem.tmp' Gemfile]
     dirs= `find test/fixtures -name Gemfile.lock`.split($/).reject(&:empty?).sort.reverse # reverse do deepest happens first
     dirs.map{|f| File.dirname f}.each {|dir|
       Dir.chdir(dir) {
         #puts "#{'-'*80}\n#{dir}\n\n"
         puts "#{dir} ..."
         system <<-EOB
-          t="#{CORVID_ROOT}/gem.tmp" \
-          && rm -f Gemfile.lock \
-          && sed -i '/^ *gem .*git:/d; $r '"$t" Gemfile \
+          rm -f Gemfile.lock \
+          && sed -i -n '/^ *gem .*git:/!p; $r #{CORVID_ROOT}/gem.tmp' Gemfile \
           && BUNDLE_GEMFILE= bundle install --local --quiet
         EOB
         puts $?.success? ? "Success." : "Failed: #{$?.exitstatus}"
