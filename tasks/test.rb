699e4c0f34e255776f91fea90e0f76662e6dad17
David Barri <japgolly@gmail.com>
2012-08-04 10:45:00 +1000

    Added Rake task for integration test

--- tasks/test.rb
+++ tasks/test.rb
@@ -1,9 +1,23 @@
-desc "Test specifications."
-task :test do
-  require 'rspec/core/rake_task'
-  RSpec::Core::RakeTask.new(:test) do |t|
-    t.rspec_path= "bin/rspec"
-    t.pattern= "test/spec{,/*,/**}/*_spec.rb"
+namespace :test do
+  def run_specs(task, dir)
+    require 'rspec/core/rake_task'
+    RSpec::Core::RakeTask.new(:"test:#{task}") do |t|
+      t.rspec_path= "bin/rspec"
+      t.pattern= "#{dir}/{,*/,**/}*_spec.rb"
+    end
   end
+
+  desc "Run tests: specifications."
+  task :spec do
+    run_specs :spec, 'test/spec'
+  end
+
+  desc "Run tests: integration."
+  task :int do
+    run_specs :int, 'test/integration'
+  end
+
 end
 
+desc "Run tests: all."
+task test: %w[test:spec test:int]
