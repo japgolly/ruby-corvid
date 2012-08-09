7c911bde29111ef7f8ad0b0a325157241bfd81dc
David Barri <japgolly@gmail.com>
2012-08-09 16:50:13 +1000

    Minor changes to respatch test

--- test/spec/res_patches_spec.rb
+++ test/spec/res_patches_spec.rb
@@ -5,30 +5,40 @@ require 'corvid/res_patch_manager'
 require 'corvid/feature_manager'
 require 'corvid/generators/update'
 
-describe "Actual resource patches" do
+describe "Actual Resources" do
   LATEST_VER= Corvid::ResPatchManager.new.latest_version
 
   run_each_in_empty_dir
 
   let(:rpm){ Corvid::ResPatchManager.new }
 
-  # Test that we can explode n->1 without errors being raised.
-  it("should be reconstructable back to v1"){
-    rpm.with_resource_versions 1 do |dir|
-      "#{dir}/1/Gemfile".should exist_as_file
-    end
-  }
+  describe "Patches" do
 
-  context 'Update vs Install' do
+    # Test that we can explode n->1 without errors being raised.
+    it("should be reconstructable back to v1"){
+      rpm.with_resource_versions 1 do |dir|
+        "#{dir}/1/Gemfile".should exist_as_file
+      end
+    }
+  end
+
+  describe 'Feature Installers: Update vs Install' do
 
     class TestInstaller < Corvid::Generator::Base
       no_tasks{
         def install(feature, ver=nil)
           ver ||= rpm.latest_version
+          # Hardcoded-logic here but all features apart from corvid, requrie corvid to be installed first.
+          unless feature == 'corvid'
+            Dir.mkdir '.corvid' unless Dir.exists?('.corvid')
+            File.write Corvid::Constants::VERSION_FILE, ver
+            File.write Corvid::Constants::FEATURES_FILE, %w[corvid].to_yaml
+          end
           with_resources(ver) {
             feature_installer!(feature).install
             add_feature feature
           }
+          File.delete Corvid::Constants::VERSION_FILE if File.exists?(Corvid::Constants::VERSION_FILE)
         end
       }
     end
