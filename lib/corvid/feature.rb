d81cfe3816e47cbdb19411b35376d721616a62b5
David Barri <japgolly@gmail.com>
2012-08-10 09:34:13 +1000

    Using attr_declarative in Feature

--- lib/corvid/feature.rb
+++ lib/corvid/feature.rb
@@ -1,21 +1,14 @@
 require 'corvid/extension'
+require 'golly-utils/attr_declarative'
 
 module Corvid
   class Feature
     include Extension
 
-    # The version of resources that this feature first appears in.
-    #
-    # @return [Fixnum] The version number.
-    def since_ver
-      1
-    end
-
-    # TODO resurrect attr_declarative?
-    def self.since_ver(ver)
-      raise "Invalid version: #{ver}" unless ver.is_a?(Fixnum) && ver > 0
-      class_eval "def since_ver; #{ver}; end"
-    end
+    # @!attribute [rw] since_ver
+    #   The version of resources that this feature first appears in.
+    #   @return [Fixnum] The version number.
+    attr_declarative since_ver: 1
 
   end
 end
