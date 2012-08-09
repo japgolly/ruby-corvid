107fa8ea553b40d39f4f8b20c011c5b70175399f
David Barri <japgolly@gmail.com>
2012-08-09 12:30:31 +1000

    clean slate

---
+++ resources/latest/corvid-features/corvid.rb
@@ -0,0 +1,6 @@
+since_ver 1
+
+install {
+  copy_file       'Rakefile'
+  empty_directory 'lib'
+}
---
+++ resources/latest/corvid-features/test_spec.rb
@@ -0,0 +1,6 @@
+since_ver 1
+
+install {
+  copy_file               'test/bootstrap/spec.rb'
+  empty_directory         'test/spec'
+}
