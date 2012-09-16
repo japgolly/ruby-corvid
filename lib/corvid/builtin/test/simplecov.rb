412b46bb71d9cf89c5492cbc5ea1081eac74b79c
David Barri <japgolly@gmail.com>
2012-09-16 14:43:20 +1000

    Fixed doco for simplecov extensions.

--- lib/corvid/builtin/test/simplecov.rb
+++ lib/corvid/builtin/test/simplecov.rb
@@ -1,32 +1,57 @@
-def add_files_to_coverage(glob_pattern, coverage_results=nil)
-  coverage_results ||= SimpleCov.result
+module Corvid
+  module Builtin
+    # This module is automatically mixed in to SimpleCov by the test bootstraps, so that in your `.simplecov` file these
+    # methods are available inside the `SimpleCov.start{ ... }` block.
+    module SimpleCovExt
 
-  # Find new files
-  new_files= Dir[glob_pattern].map{|f| File.expand_path f} - coverage_results.files.map(&:filename)
+      def add_files_to_coverage(glob_pattern, coverage_results=nil)
+        coverage_results ||= SimpleCov.result
 
-  # Map each file to an array of 0s for LOC and nils for empty (or comment-only) lines
-  nr= {}
-  new_files.each{|f| nr[f]= File.readlines(f).map{|l| /^\s*(?:#.*)?$/ === l ? nil : 0}}
+        # Find new files
+        new_files= Dir[glob_pattern].map{|f| File.expand_path f} - coverage_results.files.map(&:filename)
 
-  # Merge with existing result
-  r2= SimpleCov::Result.new nr.merge(coverage_results.original_result)
-  r2.created_at= coverage_results.created_at
-  r2.command_name= coverage_results.command_name
+        # Map each file to an array of 0s for LOC and nils for empty (or comment-only) lines
+        nr= {}
+        new_files.each{|f| nr[f]= File.readlines(f).map{|l| /^\s*(?:#.*)?$/ === l ? nil : 0}}
 
-  r2
-end
+        # Merge with existing result
+        r2= SimpleCov::Result.new nr.merge(coverage_results.original_result)
+        r2.created_at= coverage_results.created_at
+        r2.command_name= coverage_results.command_name
 
-def add_files_to_coverage_at_exit(glob_pattern)
-  SimpleCov.at_exit do
-    r2= add_files_to_coverage(glob_pattern)
-    #SimpleCov.class.instance_variable_set :@result, r2
-    r2.format!
+        r2
+      end
+
+      # Normally SimpleCov results will only include files that have been `require`d when your tests ran; if other
+      # source files exist that don't have tests and no other code loads them, then your final test coverage percentage
+      # will be incorrect.
+      #
+      # Use this method to ensure that SimpleCov knows about all source files.
+      #
+      # @example
+      #   SimpleCov.start do
+      #     add_files_to_coverage_at_exit '{app,lib}/**/*.rb'
+      #   end
+      #
+      # @param [String] glob_pattern A glob pattern that will be passed to `Dir.glob` in order to retrieve a list of source files.
+      # @return [void]
+      def add_files_to_coverage_at_exit(glob_pattern)
+        SimpleCov.at_exit do
+          r2= add_files_to_coverage(glob_pattern)
+          #SimpleCov.class.instance_variable_set :@result, r2
+          r2.format!
+        end
+      end
+
+      def skip_boring_loc_in_coverage
+        $corvid_skip_boring_lines= true
+      end
+
+    end
   end
 end
 
-def skip_boring_loc_in_coverage
-  $corvid_skip_boring_lines= true
-end
+include Corvid::Builtin::SimpleCovExt
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -38,6 +63,7 @@ SimpleCov.start unless SimpleCov.running
 # Avoid simplecov/result_merger.rb:72:in `block in store_result': undefined method `pretty_generate' for JSON:Module (NoMethodError)
 require 'json'
 
+# @!visibility private
 class SimpleCov::SourceFile
   alias :process_skipped_lines_pre_corvid :process_skipped_lines!
   def process_skipped_lines!
